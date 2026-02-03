0b1e84dd38189bb909378beb71901486e46b3250
Daniel DeFinis <definisdan@yahoo.com>
2026-02-03 00:55:09 -0500

    Add basic permissions for instructors

--- data_load/insert_permissions.sql
+++ data_load/insert_permissions.sql
@@ -5,6 +5,7 @@ INSERT INTO permissions (role_name, resource, operation, effect, cond_type, cond
 VALUES ('site_admin', 'refresh', 'read', 'allow', 'cel', 'true');
 
 /* Users */
+/* site admin */
 INSERT INTO permissions (role_name, resource, operation, effect, cond_type, cond_expr)
 VALUES ('site_admin', 'users', 'create', 'allow', 'cel', 'true');
 INSERT INTO permissions (role_name, resource, operation, effect, cond_type, cond_expr)
@@ -13,3 +14,13 @@ INSERT INTO permissions (role_name, resource, operation, effect, cond_type, cond
 VALUES ('site_admin', 'users', 'update', 'allow', 'cel', 'true');
 INSERT INTO permissions (role_name, resource, operation, effect, cond_type, cond_expr)
 VALUES ('site_admin', 'users', 'delete', 'allow', 'cel', 'true');
+
+/* instructor */
+INSERT INTO permissions (role_name, resource, operation, effect, cond_type, cond_expr)
+VALUES ('instructor', 'users', 'create', 'allow', 'cel', 'true');
+INSERT INTO permissions (role_name, resource, operation, effect, cond_type, cond_expr)
+VALUES ('instructor', 'users', 'read', 'allow', 'cel', 'true');
+INSERT INTO permissions (role_name, resource, operation, effect, cond_type, cond_expr)
+VALUES ('instructor', 'users', 'update', 'allow', 'cel', 'true');
+INSERT INTO permissions (role_name, resource, operation, effect, cond_type, cond_expr)
+VALUES ('instructor', 'users', 'delete', 'allow', 'cel', 'true');
